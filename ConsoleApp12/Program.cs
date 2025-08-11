da1f450c8c63ac4ae92d7c6d90929860b662e86e
esraa <em5938941@gmail.com>
2025-08-11 23:09:32 +0300

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Find all products that are out of stock.
+
+            //var s=from p in ListGenerator.ProductsList
+            //      where p.UnitsInStock == 0
+            //      select p;
+            //foreach (var unit in s) { 
+            //    Console.WriteLine(unit);
+
+            //}
+            #endregion
+
+            #region 2. Find all products that are in stock and cost more than 3.00 per unit.
+            //var s = from p in ListGenerator.ProductsList
+            //        where p.UnitsInStock != 0 && p.UnitPrice > 3.00m
+            //        select p;
+            //foreach (var unit in s)
+            //{
+            //    Console.WriteLine(unit);
+
+            //}
+            #endregion
+
+            #region 3. Returns digits whose name is shorter than their value.
+            string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var s = from item in Arr.Select((value, index) => new { Name = value, Index = index })
+                    where item.Name.Length < item.Index
+                    select item.Name;
+
+            foreach (var name in s)
+            {
+                Console.WriteLine(name);
+            }
+
+            #endregion
+        }
+    }
+}
